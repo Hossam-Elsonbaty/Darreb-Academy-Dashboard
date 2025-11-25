303bcf4a7035fe06845bbbcfe9871a2e619cd0bf
Mostafa Elsonbaty <armaggg3@gmail.com>
2025-11-25 22:53:07 +0200

    Refactor user model and update navigation links
    Replaced 'passwordHash' with 'password' in db.json for consistency with the new iUser interface. Added environment configuration files for development and production. Updated navbar and sidebar to use Angular router links for navigation and added a settings link. Introduced the iUser interface and refactored UsersService to use HttpClient and environment variables.

--- src/app/services/users/users.service.ts
+++ src/app/services/users/users.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { iUser } from '../../models/iUsers';
 @Injectable({
   providedIn: 'root',
 })
+
 export class UsersService {
-  
+  httpHeaders={}
+  constructor(private http:HttpClient){
+    this.httpHeaders = {
+      headers : new HttpHeaders({
+        'Content-Type':'application/json'
+      })
+    }
+  }
 }
