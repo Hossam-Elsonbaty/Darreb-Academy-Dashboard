0eaf8fc49703954c24343af461b072553dc3455d
rahmaabozied <78493571+rahmaabozied@users.noreply.github.com>
2025-11-25 14:26:27 +0200

    login
    reactive form login

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { SideBar } from "./components/side-bar/side-bar";
+import { Login } from "./components/login/login";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SideBar],
+  imports: [RouterOutlet, SideBar, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
